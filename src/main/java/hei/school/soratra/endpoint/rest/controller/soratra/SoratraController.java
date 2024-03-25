56e17958fa8a44190f42754dab7be2b0bcacc939
junkaM <hei.mikanto.3@gmail.com>
2024-03-25 07:54:48 +0100

    feat: add get Soratra Url and Transform Soratra File endpoint

--- src/main/java/hei/school/soratra/endpoint/rest/controller/soratra/SoratraController.java
+++ src/main/java/hei/school/soratra/endpoint/rest/controller/soratra/SoratraController.java
@@ -1,42 +1,34 @@
 package hei.school.soratra.endpoint.rest.controller.soratra;
 
 import hei.school.soratra.service.soratra.SoratraService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-
 import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SoratraController {
 
-    @Autowired
-    private SoratraService soratraService;
+  @Autowired private SoratraService soratraService;
 
-    @RequestMapping(
-            method = RequestMethod.PUT,
-            path = "/soratra/{id}"
-    )
-    public String uploadFile(@PathVariable String id, @RequestBody File original) throws IOException {
-        return soratraService.uploadFile(id,original);
-    }
+  @RequestMapping(method = RequestMethod.PUT, path = "/soratra/{id}")
+  public String uploadFile(@PathVariable String id, @RequestBody File original) throws IOException {
+    return soratraService.uploadFile(id, original);
+  }
 
-    @GetMapping("/soratra/{id}")
-    public Map<String, String> getSoratraUrl(@PathVariable String id) {
+  @GetMapping("/soratra/{id}")
+  public Map<String, String> getSoratraUrl(@PathVariable String id) {
 
-        String originalUrl = soratraService.getOriginalSoratraUrl(id);
-        String transformedUrl = soratraService.getTransformedSoratraUrl(id);
+    String originalUrl = soratraService.getOriginalSoratraUrl(id);
+    String transformedUrl = soratraService.getTransformedSoratraUrl(id);
 
-        Map<String, String> response = new HashMap<>();
-        response.put("original_url", originalUrl);
-        response.put("transformed_url", transformedUrl);
-        return response;
-    }
-
-
-
+    Map<String, String> response = new HashMap<>();
+    response.put("original_url", originalUrl);
+    response.put("transformed_url", transformedUrl);
+    return response;
+  }
 }
--- src/main/java/hei/school/soratra/service/soratra/SoratraService.java
+++ src/main/java/hei/school/soratra/service/soratra/SoratraService.java
@@ -1,75 +1,71 @@
 package hei.school.soratra.service.soratra;
 
 import hei.school.soratra.file.BucketComponent;
+import java.io.*;
+import java.net.URL;
+import java.time.Duration;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
-import java.net.URL;
-import java.time.Duration;
-
 @AllArgsConstructor
 @Service
 public class SoratraService {
-    @Autowired
-    private BucketComponent bucketComponent;
+  @Autowired private BucketComponent bucketComponent;
 
-    public File createFile(String name, File original) throws IOException {
-        if (name == null || name.isEmpty() || original == null) {
-            throw new IllegalArgumentException("file name or content cannot be null");
-        }
-
-
-        String path = "soratra/" + name + ".txt";
-
-        File file = new File(path);
-
-        BufferedReader reader = new BufferedReader(new FileReader(original));
-
-        BufferedWriter writer = new BufferedWriter(new FileWriter(file));
-        String line;
-        while ((line = reader.readLine()) != null) {
-            String upperCaseLine = line.toUpperCase();
-            writer.write(upperCaseLine);
-            writer.newLine();
-        }
-        reader.close();
-        writer.close();
-
-        return file;
-    }
-    public String uploadFile(String id, File original) throws IOException {
-
-        String originalImagePrefix = id + "-original";
-        String transformedImagePrefix = id + "-transformed";
-
-        String originalBucketKey = "soratra/" + originalImagePrefix + "txt";
-        String transformedBucketKey = "soratra/" + transformedImagePrefix + "txt";
-
-
-
-        File file = this.createFile(id,original);
-
-        bucketComponent.upload(original,originalBucketKey);
-        bucketComponent.upload(file,transformedBucketKey);
-
-        return bucketComponent.presign(transformedBucketKey, Duration.ofMinutes(30)).toString();
+  public File createFile(String name, File original) throws IOException {
+    if (name == null || name.isEmpty() || original == null) {
+      throw new IllegalArgumentException("file name or content cannot be null");
     }
 
-    public String getOriginalSoratraUrl(String id) {
-        String originalImagePrefix = id + "-original";
-        String originalBucketKey = "soratra/" + originalImagePrefix + "txt";
+    String path = "soratra/" + name + ".txt";
 
-        URL originalUrl = bucketComponent.presign(originalBucketKey, Duration.ofMinutes(20));
-        return originalUrl.toString();
-    }
+    File file = new File(path);
 
-    public String getTransformedSoratraUrl(String id) {
-        String transformedImagePrefix = id + "-transformed";
-        String transformedBucketKey = "soratra/" + transformedImagePrefix + "txt";
-        
-        URL transformedUrl = bucketComponent.presign(transformedBucketKey, Duration.ofMinutes(20));
-        return transformedUrl.toString();
+    BufferedReader reader = new BufferedReader(new FileReader(original));
+
+    BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+    String line;
+    while ((line = reader.readLine()) != null) {
+      String upperCaseLine = line.toUpperCase();
+      writer.write(upperCaseLine);
+      writer.newLine();
     }
+    reader.close();
+    writer.close();
+
+    return file;
+  }
+
+  public String uploadFile(String id, File original) throws IOException {
+
+    String originalImagePrefix = id + "-original";
+    String transformedImagePrefix = id + "-transformed";
+
+    String originalBucketKey = "soratra/" + originalImagePrefix + "txt";
+    String transformedBucketKey = "soratra/" + transformedImagePrefix + "txt";
+
+    File file = this.createFile(id, original);
+
+    bucketComponent.upload(original, originalBucketKey);
+    bucketComponent.upload(file, transformedBucketKey);
+
+    return bucketComponent.presign(transformedBucketKey, Duration.ofMinutes(30)).toString();
+  }
+
+  public String getOriginalSoratraUrl(String id) {
+    String originalImagePrefix = id + "-original";
+    String originalBucketKey = "soratra/" + originalImagePrefix + "txt";
+
+    URL originalUrl = bucketComponent.presign(originalBucketKey, Duration.ofMinutes(20));
+    return originalUrl.toString();
+  }
+
+  public String getTransformedSoratraUrl(String id) {
+    String transformedImagePrefix = id + "-transformed";
+    String transformedBucketKey = "soratra/" + transformedImagePrefix + "txt";
+
+    URL transformedUrl = bucketComponent.presign(transformedBucketKey, Duration.ofMinutes(20));
+    return transformedUrl.toString();
+  }
 }
